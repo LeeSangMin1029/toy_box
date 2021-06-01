afc259c1e02d1eec1b5e92d0a2aebecddb1bcc40
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-02 02:16:46 +0900

    여러개의 파티클을 관리하기 위한 제어 클래스

---
+++ javascripts/handler.js
@@ -0,0 +1,43 @@
+import Particle from './particle.js';
+class HandleParticle {
+  #particles;
+  #ctx;
+  constructor() {
+    this.#particles = [];
+  }
+  set ctx(ctx) {
+    this.#ctx = ctx;
+  }
+  get ctx() {
+    return this.#ctx;
+  }
+  add() {
+    this.#particles.push(new Particle());
+  }
+  update() {
+    for (let i = 0; i < this.#particles.length; i++) {
+      this.#particles[i].update();
+      this.#particles[i].draw(this.#ctx);
+      for (let j = i; j < this.#particles.length; j++) {
+        const dx = this.#particles[i].x - this.#particles[j].x;
+        const dy = this.#particles[i].y - this.#particles[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < 100) {
+          this.#ctx.beginPath();
+          this.#ctx.strokeStyle = this.#particles[i].color;
+          this.#ctx.lineWidth = 1;
+          this.#ctx.moveTo(this.#particles[i].x, this.#particles[i].y);
+          this.#ctx.lineTo(this.#particles[j].x, this.#particles[j].y);
+          this.#ctx.stroke();
+          this.#ctx.closePath();
+        }
+      }
+      if (this.#particles[i].size <= 0.2) {
+        this.#particles.splice(i, 1);
+        i--;
+      }
+    }
+  }
+}
+const particleHandler = new HandleParticle();
+export { particleHandler };
