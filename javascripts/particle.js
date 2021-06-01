100ede9a8de0014f3019d3195945bfe13dc091ca
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-02 02:16:20 +0900

    하나의 파티클의 속성을 담고 있는 클래스

---
+++ javascripts/particle.js
@@ -0,0 +1,40 @@
+import { mouse, color } from './object.js';
+export default class Particle {
+  #x;
+  #y;
+  #size;
+  #speedX;
+  #speedY;
+  #color;
+  constructor() {
+    this.#x = mouse.x;
+    this.#y = mouse.y;
+    this.#size = Math.random() * 10 + 1;
+    this.#speedX = Math.random() * 2 - 1.0;
+    this.#speedY = Math.random() * 2 - 1.0;
+    this.#color = `hsl(${color.hue}, 100%, 50%)`;
+  }
+  get x() {
+    return this.#x;
+  }
+  get y() {
+    return this.#y;
+  }
+  get size() {
+    return this.#size;
+  }
+  get color() {
+    return this.#color;
+  }
+  update() {
+    this.#x += this.#speedX;
+    this.#y += this.#speedY;
+    if (this.#size > 0.2) this.#size -= 0.1;
+  }
+  draw(ctx) {
+    ctx.fillStyle = this.#color;
+    ctx.beginPath();
+    ctx.arc(this.#x, this.#y, this.#size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
