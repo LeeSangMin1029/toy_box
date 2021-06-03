a81b1a5cf557df78d1fdb19f1234d1cdc3210435
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-04 00:33:39 +0900

    전체 시간을 조정하기 위한 클로저 객체 생성

--- follow_pointer/javascripts/object.js
+++ follow_pointer/javascripts/object.js
@@ -9,4 +9,26 @@ const mouse = {
 const color = {
   hue: 0,
 };
-export { mouse, color };
+const time = (function () {
+  let speed = 1.0;
+  function changeBy(val) {
+    speed += val;
+    if (speed > 2) {
+      speed = 2;
+    } else if (speed < 0) {
+      speed = 0;
+    }
+  }
+  return {
+    up: () => {
+      changeBy(0.1);
+    },
+    down: () => {
+      changeBy(-0.1);
+    },
+    getSpeed: () => {
+      return speed;
+    },
+  };
+})();
+export { mouse, color, time };
